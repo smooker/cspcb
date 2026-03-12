4d6018b35e4afb15bac3edcf2316f4458619db69
smooker <miro@smooker.org>
2026-03-12 13:17:21 +0200

    pushed from sw2

--- firmware/USB_DEVICE/Target/usbd_conf.c
+++ firmware/USB_DEVICE/Target/usbd_conf.c
@@ -625,7 +625,6 @@ USBD_StatusTypeDef USBD_LL_SetTestMode(USBD_HandleTypeDef *pdev, uint8_t testmod
   */
 void *USBD_static_malloc(uint32_t size)
 {
-    UNUSED(size);
   static uint32_t mem[(sizeof(USBD_CDC_HandleTypeDef)/4)+1];/* On 32-bit boundary */
   return mem;
 }
@@ -637,7 +636,7 @@ void *USBD_static_malloc(uint32_t size)
   */
 void USBD_static_free(void *p)
 {
-    UNUSED(p);
+
 }
 
 /**
